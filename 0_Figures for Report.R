b85495a9e36e2d572239641a0a05a5c062bb24bc
Sheena Martenies <sheena.martenies@colostate.edu>
2018-09-05 13:20:08 -0600

    updating figures for the report

--- 0_Figures for Report.R
+++ 0_Figures for Report.R
@@ -188,11 +188,13 @@ co_cities <- st_read("T:/Rsch-MRS/ECHO/SEM Large Data/Spatial Data/MuniBounds.sh
   st_transform(albers)
 plot(st_geometry(co_cities)) 
 
-pueblo <- filter(co_cities, first_city == "Pueblo")
+pueblo <- filter(co_cities, first_city == "Pueblo") %>% 
+  st_transform(ll_wgs84)
 plot(st_geometry(pueblo))
 
-cos <- filter(co_cities, first_city == "Colorado Springs")
-plot(st_geometry(cos))
+springs <- filter(co_cities, first_city == "Colorado Springs") %>% 
+  st_transform(ll_wgs84)
+plot(st_geometry(springs))
 
 #' sf object with all of the variables
 
@@ -212,9 +214,8 @@ load("./HIA Outputs/HIA_Winter_HIA_Summer_zcta_combined.RData")
 impacts <- select(combined_df, zcta, pol, outcome, median_scaled) %>% 
   left_join(ses, by="zcta") %>% 
   mutate(rate = (median_scaled / total_pop) * rate_pop) %>% 
-  filter(outcome %in% c("mort_ac", "st_mort_na", "hosp_res", "hosp_cvd")) %>% 
+  filter(outcome %in% c("mort_ac", "st_mort_na", "hosp_res", "hosp_cvd")) 
   
-
 load("./Data/Spatial Data/co_zcta.RData")
 zcta_impacts <- filter(co_zcta, GEOID10 %in% zcta_ids) %>%
   select(GEOID10) %>%
@@ -222,113 +223,139 @@ zcta_impacts <- filter(co_zcta, GEOID10 %in% zcta_ids) %>%
   left_join(impacts, by="zcta") %>% 
   st_transform(ll_wgs84)
 
+pueblo_zips <- c("81001", "81003", "81004", "81005", "81006",
+                 "81007", "81008", "81022", "81025")
+
+pueblo_impacts <- filter(zcta_impacts, zcta %in% pueblo_zips)
+plot(st_geometry(pueblo_impacts))
+plot(st_geometry(pueblo), border="red", add=T)
+
+springs_zips <- c("80938", "80939", "80951", "80918", "80919", "80920",
+                  "80921", "80922", "80923", "80924", "80925", "80926",
+                  "80927", "80929", "80903", "80904", "80905", "80906", 
+                  "80907", "80908", "80909", "80910", "80911", "80914",
+                  "80915", "80916", "80917", "80809", "80829", "80831",
+                  "80840", "80913", "80817", "80925", "80929")
+
+springs_impacts <- filter(zcta_impacts, zcta %in% springs_zips)
+plot(st_geometry(springs_impacts))
+plot(st_geometry(springs), border="red", add=T)
+
 #' Maps for Pueblo
-ggplot(data = zcta_impacts) +
+ggplot(data = pueblo_impacts) +
   ggtitle(paste("Persons that are non-Hispanic white alone")) +
   geom_sf(aes(fill = pct_nhw), color = NA) +
   geom_sf(data = pueblo, fill=NA, color="red") +
-  geom_sf(data = pp, color = "black", size = 3) +
+  geom_sf(data = pp, color = "lightblue", size = 3) +
   scale_fill_viridis(name = "Percentage") +
   simple_theme +
   theme(legend.position = "bottom",
+        legend.text = element_text(angle = 90, hjust = 1),
         axis.text = element_blank(),
         axis.ticks = element_blank()) +
   coord_sf(crs = ll_wgs84, xlim = c(-105.25, -103.75), ylim = c(37.6, 38.7))
 ggsave(filename = "C:/Users/semarten/Dropbox/ALA_HIA/Report/Figures/Pueblo NHW.jpeg",
        device = "jpeg", dpi = 600, units = "in", height = 5, width = 5)
   
-ggplot(data = zcta_impacts) +
+ggplot(data = pueblo_impacts) +
   ggtitle(paste("Persons of color")) +
   geom_sf(aes(fill = pct_poc), color = NA) +
   geom_sf(data = pueblo, fill=NA, color="red") +
-  geom_sf(data = pp, color = "black", size = 3) +
+  geom_sf(data = pp, color = "lightblue", size = 3) +
   scale_fill_viridis(name = "Percentage") +
   simple_theme +
   theme(legend.position = "bottom",
+        legend.text = element_text(angle = 90, hjust = 1),
         axis.text = element_blank(),
         axis.ticks = element_blank()) +
   coord_sf(crs = ll_wgs84, xlim = c(-105.25, -103.75), ylim = c(37.6, 38.7))
 ggsave(filename = "C:/Users/semarten/Dropbox/ALA_HIA/Report/Figures/Pueblo POC.jpeg",
        device = "jpeg", dpi = 600, units = "in", height = 5, width = 5)
 
-ggplot(data = zcta_impacts) +
+ggplot(data = pueblo_impacts) +
   ggtitle(paste("Median income")) +
   geom_sf(aes(fill = med_income/10000), color = NA) +
   geom_sf(data = pueblo, fill=NA, color="red") +
-  geom_sf(data = pp, color = "black", size = 3) +
+  geom_sf(data = pp, color = "lightblue", size = 3) +
   scale_fill_viridis(name = "2014$ (10,000's)") +
   simple_theme +
   theme(legend.position = "bottom",
+        legend.text = element_text(angle = 90, hjust = 1),
         axis.text = element_blank(),
         axis.ticks = element_blank()) +
   coord_sf(crs = ll_wgs84, xlim = c(-105.25, -103.75), ylim = c(37.6, 38.7))
 ggsave(filename = "C:/Users/semarten/Dropbox/ALA_HIA/Report/Figures/Pueblo Income.jpeg",
        device = "jpeg", dpi = 600, units = "in", height = 5, width = 5)
 
-ggplot(data = filter(zcta_impacts, pol == "pm" & outcome == "mort_ac")) +
+ggplot(data = filter(pueblo_impacts, pol == "pm" & outcome == "mort_ac")) +
   ggtitle(paste("Avoided premature deaths (PM\u2082.\u2085)")) +
   geom_sf(aes(fill = rate), color = NA) +
   geom_sf(data = pueblo, fill=NA, color="red") +
-  geom_sf(data = pp, color = "black", size = 3) +
+  geom_sf(data = pp, color = "lightblue", size = 3) +
   scale_fill_viridis(name = "Rate per 10,000") +
   simple_theme +
   theme(legend.position = "bottom",
+        legend.text = element_text(angle = 90, hjust = 1),
         axis.text = element_blank(),
         axis.ticks = element_blank()) +
   coord_sf(crs = ll_wgs84, xlim = c(-105.25, -103.75), ylim = c(37.6, 38.7))
 ggsave(filename = "C:/Users/semarten/Dropbox/ALA_HIA/Report/Figures/Pueblo Avoided PM Deaths.jpeg",
        device = "jpeg", dpi = 600, units = "in", height = 5, width = 5)
 
-ggplot(data = filter(zcta_impacts, pol == "pm" & outcome == "hosp_cvd")) +
+ggplot(data = filter(pueblo_impacts, pol == "pm" & outcome == "hosp_cvd")) +
   ggtitle(paste("Avoided CVD hospitalizations (PM\u2082.\u2085)")) +
   geom_sf(aes(fill = rate), color = NA) +
   geom_sf(data = pueblo, fill=NA, color="red") +
-  geom_sf(data = pp, color = "black", size = 3) +
+  geom_sf(data = pp, color = "lightblue", size = 3) +
   scale_fill_viridis(name = "Rate per 10,000") +
   simple_theme +
   theme(legend.position = "bottom",
+        legend.text = element_text(angle = 90, hjust = 1),
         axis.text = element_blank(),
         axis.ticks = element_blank()) +
   coord_sf(crs = ll_wgs84, xlim = c(-105.25, -103.75), ylim = c(37.6, 38.7))
 ggsave(filename = "C:/Users/semarten/Dropbox/ALA_HIA/Report/Figures/Pueblo Avoided PM CVD.jpeg",
        device = "jpeg", dpi = 600, units = "in", height = 5, width = 5)
 
-ggplot(data = filter(zcta_impacts, pol == "pm" & outcome == "hosp_res")) +
+ggplot(data = filter(pueblo_impacts, pol == "pm" & outcome == "hosp_res")) +
   ggtitle(paste("Avoided RES hospitalizations (PM\u2082.\u2085)")) +
   geom_sf(aes(fill = rate), color = NA) +
   geom_sf(data = pueblo, fill=NA, color="red") +
-  geom_sf(data = pp, color = "black", size = 3) +
+  geom_sf(data = pp, color = "lightblue", size = 3) +
   scale_fill_viridis(name = "Rate per 10,000") +
   simple_theme +
   theme(legend.position = "bottom",
+        legend.text = element_text(angle = 90, hjust = 1),
         axis.text = element_blank(),
         axis.ticks = element_blank()) +
   coord_sf(crs = ll_wgs84, xlim = c(-105.25, -103.75), ylim = c(37.6, 38.7))
 ggsave(filename = "C:/Users/semarten/Dropbox/ALA_HIA/Report/Figures/Pueblo Avoided PM RES.jpeg",
        device = "jpeg", dpi = 600, units = "in", height = 5, width = 5)
 
-ggplot(data = filter(zcta_impacts, pol == "o3" & outcome == "st_mort_na")) +
+ggplot(data = filter(pueblo_impacts, pol == "o3" & outcome == "st_mort_na")) +
   ggtitle(paste("Avoided premature deaths (O\u2083)")) +
   geom_sf(aes(fill = rate), color = NA) +
   geom_sf(data = pueblo, fill=NA, color="red") +
-  geom_sf(data = pp, color = "black", size = 3) +
+  geom_sf(data = pp, color = "lightblue", size = 3) +
   scale_fill_viridis(name = "Rate per 10,000") +
   simple_theme +
   theme(legend.position = "bottom",
+        legend.text = element_text(angle = 90, hjust = 1),
         axis.text = element_blank(),
         axis.ticks = element_blank()) +
   coord_sf(crs = ll_wgs84, xlim = c(-105.25, -103.75), ylim = c(37.6, 38.7))
 ggsave(filename = "C:/Users/semarten/Dropbox/ALA_HIA/Report/Figures/Pueblo Avoided O3 Deaths.jpeg",
        device = "jpeg", dpi = 600, units = "in", height = 5, width = 5)
 
-ggplot(data = filter(zcta_impacts, pol == "o3" & outcome == "hosp_res")) +
+ggplot(data = filter(pueblo_impacts, pol == "o3" & outcome == "hosp_res")) +
   ggtitle(paste("Avoided RES hospitalizations (O\u2083)")) +
   geom_sf(aes(fill = rate), color = NA) +
   geom_sf(data = pueblo, fill=NA, color="red") +
-  geom_sf(data = pp, color = "black", size = 3) +
+  geom_sf(data = pp, color = "lightblue", size = 3) +
   scale_fill_viridis(name = "Rate per 10,000") +
   simple_theme +
   theme(legend.position = "bottom",
+        legend.text = element_text(angle = 90, hjust = 1),
         axis.text = element_blank(),
         axis.ticks = element_blank()) +
   coord_sf(crs = ll_wgs84, xlim = c(-105.25, -103.75), ylim = c(37.6, 38.7))
@@ -336,114 +363,122 @@ ggsave(filename = "C:/Users/semarten/Dropbox/ALA_HIA/Report/Figures/Pueblo Avoid
        device = "jpeg", dpi = 600, units = "in", height = 5, width = 5)
 
 #' Maps for Colorado Springs
-ggplot(data = zcta_impacts) +
+ggplot(data = springs_impacts) +
   ggtitle(paste("Persons that are non-Hispanic white alone")) +
   geom_sf(aes(fill = pct_nhw), color = NA) +
-  geom_sf(data = cos, fill=NA, color="red") +
-  geom_sf(data = pp, color = "black", size = 3) +
+  geom_sf(data = springs, fill=NA, color="red") +
+  geom_sf(data = pp, color = "lightblue", size = 3) +
   scale_fill_viridis(name = "Percentage") +
   simple_theme +
   theme(legend.position = "bottom",
+        legend.text = element_text(angle = 90, hjust = 1),
         axis.text = element_blank(),
         axis.ticks = element_blank()) +
-  coord_sf(crs = ll_wgs84, xlim = c(-105.5, -104), ylim = c(38.2, 39.3))
+  coord_sf(crs = ll_wgs84, xlim = c(-105.4, -104.1), ylim = c(38.3, 39.2))
 ggsave(filename = "C:/Users/semarten/Dropbox/ALA_HIA/Report/Figures/Colorado Springs NHW.jpeg",
        device = "jpeg", dpi = 600, units = "in", height = 5, width = 5)
 
-ggplot(data = zcta_impacts) +
+ggplot(data = springs_impacts) +
   ggtitle(paste("Persons of color")) +
   geom_sf(aes(fill = pct_poc), color = NA) +
-  geom_sf(data = cos, fill=NA, color="red") +
-  geom_sf(data = pp, color = "black", size = 3) +
+  geom_sf(data = springs, fill=NA, color="red") +
+  geom_sf(data = pp, color = "lightblue", size = 3) +
   scale_fill_viridis(name = "Percentage") +
   simple_theme +
   theme(legend.position = "bottom",
+        legend.text = element_text(angle = 90, hjust = 1),
         axis.text = element_blank(),
         axis.ticks = element_blank()) +
-  coord_sf(crs = ll_wgs84, xlim = c(-105.5, -104), ylim = c(38.2, 39.3))
+  coord_sf(crs = ll_wgs84, xlim = c(-105.4, -104.1), ylim = c(38.3, 39.2))
 ggsave(filename = "C:/Users/semarten/Dropbox/ALA_HIA/Report/Figures/Colorado Springs POC.jpeg",
        device = "jpeg", dpi = 600, units = "in", height = 5, width = 5)
 
-ggplot(data = zcta_impacts) +
+ggplot(data = springs_impacts) +
   ggtitle(paste("Median income")) +
   geom_sf(aes(fill = med_income/10000), color = NA) +
-  geom_sf(data = cos, fill=NA, color="red") +
-  geom_sf(data = pp, color = "black", size = 3) +
+  geom_sf(data = springs, fill=NA, color="red") +
+  geom_sf(data = pp, color = "lightblue", size = 3) +
   scale_fill_viridis(name = "2014$ (10,000's)") +
   simple_theme +
   theme(legend.position = "bottom",
+        legend.text = element_text(angle = 90, hjust = 1),
         axis.text = element_blank(),
         axis.ticks = element_blank()) +
-  coord_sf(crs = ll_wgs84, xlim = c(-105.5, -104), ylim = c(38.2, 39.3))
+  coord_sf(crs = ll_wgs84, xlim = c(-105.4, -104.1), ylim = c(38.3, 39.2))
 ggsave(filename = "C:/Users/semarten/Dropbox/ALA_HIA/Report/Figures/Colorado Springs Income.jpeg",
        device = "jpeg", dpi = 600, units = "in", height = 5, width = 5)
 
-ggplot(data = filter(zcta_impacts, pol == "pm" & outcome == "mort_ac")) +
+ggplot(data = filter(springs_impacts, pol == "pm" & outcome == "mort_ac")) +
   ggtitle(paste("Avoided premature deaths (PM\u2082.\u2085)")) +
   geom_sf(aes(fill = rate), color = NA) +
-  geom_sf(data = cos, fill=NA, color="red") +
-  geom_sf(data = pp, color = "black", size = 3) +
+  geom_sf(data = springs, fill=NA, color="red") +
+  geom_sf(data = pp, color = "lightblue", size = 3) +
   scale_fill_viridis(name = "Rate per 10,000") +
   simple_theme +
   theme(legend.position = "bottom",
+        legend.text = element_text(angle = 90, hjust = 1),
         axis.text = element_blank(),
         axis.ticks = element_blank()) +
-  coord_sf(crs = ll_wgs84, xlim = c(-105.5, -104), ylim = c(38.2, 39.3))
+  coord_sf(crs = ll_wgs84, xlim = c(-105.4, -104.1), ylim = c(38.3, 39.2))
 ggsave(filename = "C:/Users/semarten/Dropbox/ALA_HIA/Report/Figures/Colorado Springs Avoided PM Deaths.jpeg",
        device = "jpeg", dpi = 600, units = "in", height = 5, width = 5)
 
-ggplot(data = filter(zcta_impacts, pol == "pm" & outcome == "hosp_cvd")) +
+ggplot(data = filter(springs_impacts, pol == "pm" & outcome == "hosp_cvd")) +
   ggtitle(paste("Avoided CVD hospitalizations (PM\u2082.\u2085)")) +
   geom_sf(aes(fill = rate), color = NA) +
-  geom_sf(data = cos, fill=NA, color="red") +
-  geom_sf(data = pp, color = "black", size = 3) +
+  geom_sf(data = springs, fill=NA, color="red") +
+  geom_sf(data = pp, color = "lightblue", size = 3) +
   scale_fill_viridis(name = "Rate per 10,000") +
   simple_theme +
   theme(legend.position = "bottom",
+        legend.text = element_text(angle = 90, hjust = 1),
         axis.text = element_blank(),
         axis.ticks = element_blank()) +
-  coord_sf(crs = ll_wgs84, xlim = c(-105.5, -104), ylim = c(38.2, 39.3))
+  coord_sf(crs = ll_wgs84, xlim = c(-105.4, -104.1), ylim = c(38.3, 39.2))
 ggsave(filename = "C:/Users/semarten/Dropbox/ALA_HIA/Report/Figures/Colorado Springs Avoided PM CVD.jpeg",
        device = "jpeg", dpi = 600, units = "in", height = 5, width = 5)
 
-ggplot(data = filter(zcta_impacts, pol == "pm" & outcome == "hosp_res")) +
+ggplot(data = filter(springs_impacts, pol == "pm" & outcome == "hosp_res")) +
   ggtitle(paste("Avoided RES hospitalizations (PM\u2082.\u2085)")) +
   geom_sf(aes(fill = rate), color = NA) +
-  geom_sf(data = cos, fill=NA, color="red") +
-  geom_sf(data = pp, color = "black", size = 3) +
+  geom_sf(data = springs, fill=NA, color="red") +
+  geom_sf(data = pp, color = "lightblue", size = 3) +
   scale_fill_viridis(name = "Rate per 10,000") +
   simple_theme +
   theme(legend.position = "bottom",
+        legend.text = element_text(angle = 90, hjust = 1),
         axis.text = element_blank(),
         axis.ticks = element_blank()) +
-  coord_sf(crs = ll_wgs84, xlim = c(-105.5, -104), ylim = c(38.2, 39.3))
+  coord_sf(crs = ll_wgs84, xlim = c(-105.4, -104.1), ylim = c(38.3, 39.2))
 ggsave(filename = "C:/Users/semarten/Dropbox/ALA_HIA/Report/Figures/Colorado Springs Avoided PM RES.jpeg",
        device = "jpeg", dpi = 600, units = "in", height = 5, width = 5)
 
-ggplot(data = filter(zcta_impacts, pol == "o3" & outcome == "st_mort_na")) +
+ggplot(data = filter(springs_impacts, pol == "o3" & outcome == "st_mort_na")) +
   ggtitle(paste("Avoided premature deaths (O\u2083)")) +
   geom_sf(aes(fill = rate), color = NA) +
-  geom_sf(data = cos, fill=NA, color="red") +
-  geom_sf(data = pp, color = "black", size = 3) +
+  geom_sf(data = springs, fill=NA, color="red") +
+  geom_sf(data = pp, color = "lightblue", size = 3) +
   scale_fill_viridis(name = "Rate per 10,000") +
   simple_theme +
   theme(legend.position = "bottom",
+        legend.text = element_text(angle = 90, hjust = 1),
         axis.text = element_blank(),
         axis.ticks = element_blank()) +
-  coord_sf(crs = ll_wgs84, xlim = c(-105.5, -104), ylim = c(38.2, 39.3))
+  coord_sf(crs = ll_wgs84, xlim = c(-105.4, -104.1), ylim = c(38.3, 39.2))
 ggsave(filename = "C:/Users/semarten/Dropbox/ALA_HIA/Report/Figures/Colorado Springs Avoided O3 Deaths.jpeg",
        device = "jpeg", dpi = 600, units = "in", height = 5, width = 5)
 
-ggplot(data = filter(zcta_impacts, pol == "o3" & outcome == "hosp_res")) +
+ggplot(data = filter(springs_impacts, pol == "o3" & outcome == "hosp_res")) +
   ggtitle(paste("Avoided RES hospitalizations (O\u2083)")) +
   geom_sf(aes(fill = rate), color = NA) +
-  geom_sf(data = cos, fill=NA, color="red") +
-  geom_sf(data = pp, color = "black", size = 3) +
+  geom_sf(data = springs, fill=NA, color="red") +
+  geom_sf(data = pp, color = "lightblue", size = 3) +
   scale_fill_viridis(name = "Rate per 10,000") +
   simple_theme +
   theme(legend.position = "bottom",
+        legend.text = element_text(angle = 90, hjust = 1),
         axis.text = element_blank(),
         axis.ticks = element_blank()) +
-  coord_sf(crs = ll_wgs84, xlim = c(-105.5, -104), ylim = c(38.2, 39.3))
+  coord_sf(crs = ll_wgs84, xlim = c(-105.4, -104.1), ylim = c(38.3, 39.2))
 ggsave(filename = "C:/Users/semarten/Dropbox/ALA_HIA/Report/Figures/Colorado Springs Avoided O3 RES.jpeg",
        device = "jpeg", dpi = 600, units = "in", height = 5, width = 5)
